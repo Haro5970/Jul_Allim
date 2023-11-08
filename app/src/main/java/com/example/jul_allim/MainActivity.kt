a2984d70d8eaace6573896cbb6790595a5f8898f
Haro <tootoofast0571@gmail.com>
2023-11-08 21:32:15 +0900

    지훈 공지화면

--- app/src/main/java/com/example/jul_allim/MainActivity.kt
+++ app/src/main/java/com/example/jul_allim/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.jul_allim
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.jul_allim.databinding.ActivityMainBinding
 
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity() {
             // 화면 초기화
         binding = ActivityMainBinding.inflate(layoutInflater)
         setMainFragment(NotionFragment(),"공지사항")
+        setUnderbarColor(1)
         setContentView(binding.root)
 
 
@@ -32,16 +36,20 @@ class MainActivity : AppCompatActivity() {
             // 하단바 버튼 클릭시 페이지 이동
         binding.btn1.setOnClickListener {
             setMainFragment(NotionFragment(),"공지사항")
+            setUnderbarColor(1)
         }
         binding.btn2.setOnClickListener {
             setMainFragment(CalenderFragment(),"캘린더")
+            setUnderbarColor(2)
         }
         binding.btn3.setOnClickListener {
             setMainFragment(ClubReservationFragment(),"동방예약")
+            setUnderbarColor(3)
         }
 
         binding.btn5.setOnClickListener {
             setMainFragment(ProfileFragment(), "프로필 설정")
+            setUnderbarColor(5)
         }
 
     }
@@ -60,6 +68,19 @@ class MainActivity : AppCompatActivity() {
         binding.textTitle.text = title
     }
 
+    fun setUnderbarColor( btnN: Int){
+        val btn_arr=arrayOf(binding.btn1,binding.btn2,binding.btn3,binding.btn4,binding.btn5)
+        for( i:Int in 1..5){
+            if(btnN==i){
+                btn_arr[i-1].imageTintList =
+                    ColorStateList.valueOf(ContextCompat.getColor(this, R.color.black))
+            }
+            else{
+                btn_arr[i-1].imageTintList =
+                    ColorStateList.valueOf(ContextCompat.getColor(this, R.color.gray))
+            }
+        }
+    }
     // 캘린더뷰
 
 
