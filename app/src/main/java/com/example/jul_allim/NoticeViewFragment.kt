342f30d09c9f15052263eb725132436ef5f36915
Haro <tootoofast0571@gmail.com>
2023-11-30 11:23:20 +0900

    Haro

--- app/src/main/java/com/example/jul_allim/NoticeViewFragment.kt
+++ app/src/main/java/com/example/jul_allim/NoticeViewFragment.kt
@@ -2,11 +2,13 @@ package com.example.jul_allim
 
 import android.graphics.Bitmap
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ScrollingView
 import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.LiveData
@@ -23,6 +25,7 @@ class NoticeViewFragment(val notice: Notice) : Fragment() {
         val binding = FragmentNoticeViewBinding.inflate(inflater,container,false)
         val viewModel: NoticeViewModel by activityViewModels()
 
+        binding.noticeView.movementMethod = ScrollingMovementMethod.getInstance()
         binding.noticeId.text = notice.id
         binding.noticeView.text = notice.lines
         binding.bigImgView.isVisible = false
@@ -34,8 +37,9 @@ class NoticeViewFragment(val notice: Notice) : Fragment() {
             binding.bigImgView.setOnClickListener { binding.bigImgView.isVisible=false }
             if(it.size>0){
                 binding.noticeImgs.apply {
-                    adapter = NoticeImageAdapter(it,
-                        { binding.bigImgView.isVisible = true; binding.bigImgView.setImageBitmap(it); })
+                    adapter = NoticeImageAdapter(it) {
+                        binding.bigImgView.isVisible = true
+                        binding.bigImgView.setImageBitmap(it) }
                     layoutManager = LinearLayoutManager(requireContext()).also { it.orientation = LinearLayoutManager.HORIZONTAL }
                 }
             }
